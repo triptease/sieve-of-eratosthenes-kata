b33f99c35214c24c7bec88a9675ed4584430bc1c
Sam Firminger <samfirminger@gmail.com>
2022-03-31 14:03:19 +0100

    first pass

--- src/sieve.ts
+++ src/sieve.ts
@@ -1,3 +1,17 @@
-export function primes() {
-    throw new Error('Remove this statement and implement this function')
+export function primes(limit: number): number[] {
+
+  let array = [];
+  for (let i = 2; i <= limit; i++) {
+    array.push(i);
+  }
+
+  for (let i = 2; i < limit; i++) {
+    let multiples = [];
+    for (let j = 2; j < limit; j++) {
+      multiples.push(i*j);
+    }
+    array = array.filter(num => !multiples.includes(num));
+  }
+
+  return array;
 }
